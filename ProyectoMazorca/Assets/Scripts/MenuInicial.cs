70ed72d5018a9b52950698eca9a0931a2415cfd8
CarlosCC27 <al425984@uji.es>
2025-10-17 18:37:10 +0200

    Menú inicial y cambio de mapa

---
+++ ProyectoMazorca/Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Salir(){
+        Application.Quit();
+    }
+}
